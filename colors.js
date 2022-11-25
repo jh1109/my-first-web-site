78009034583006e571b55b26064faa3b9cca0caa
jh1109 <117807467+jh1109@users.noreply.github.com>
2022-11-25 11:49:28 +0900

    생활코딩_WEB2_JavaScript(6)
    - night & day 기능을 colors.js로 만들어 전체파일에 적용
    - 미리어쿼리 코드 오류 수정(빅터프랭클 북리뷰 페이지 사진 정상화)
    - 라이브러리 : jQuery 학습
    - 리팩토링

---
+++ colors.js
@@ -0,0 +1,33 @@
+function aSetColor(color){
+  var i = 1;
+  var alist = document.querySelectorAll('a');
+  while(i < alist.length){
+    alist[i].style.color = color;
+    i = i + 1;
+  }
+  // $('a').css('color', color);
+}
+var Body = {
+  setColor:function (color){
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
+  },
+  setBackgroundColor:function (color){
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+  }
+}
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+    aSetColor('white');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+    aSetColor('black');
+  }
+}
